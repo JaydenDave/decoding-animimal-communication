7f0ce6412b280c03f187ad69c1dd0714593262ea
Jayden Dave <jaydendave@hotmail.co.uk>
2023-11-24 16:44:19 +0000

    made ciwgan

---
+++ winfoGAN/preprocess.py
@@ -0,0 +1,77 @@
+import numpy as np
+import os
+import librosa as lb
+import pickle
+import random
+
+
+def set_duration(signal, max):
+    if len(signal) < max:
+        num_missing_samples = max - len(signal)
+        padded_array = np.pad(signal,
+                              (0,num_missing_samples),
+                              mode = "constant")
+        return padded_array
+    else:
+        signal = signal[:max]
+    return signal
+
+def denormalise(samples, normaliser_file_path):
+    with open(normaliser_file_path, 'rb') as f:
+        norm_vals = pickle.load(f)
+    mean = norm_vals["mean"]
+    std_dev = norm_vals["std_dev"]
+    print(f"DENORMALISING- MEAN:{mean}, STD_DEV:{std_dev}")
+    samples = (samples * std_dev) + mean
+    print("DENORMALISED")
+    return samples
+
+
+def z_score_normalise(array, save_path = "."):
+    flattened_array = array.flatten()
+
+    # Calculate the mean and standard deviation of the entire set
+    mean_value = np.mean(flattened_array)
+    std_deviation = np.std(flattened_array)
+
+    # Z-score normalize each audio clip individually
+    normalized_samples = (array - mean_value) / std_deviation
+    norm_vals = {"mean": mean_value, "std_dev": std_deviation}
+    with open(f"{save_path}/normaliser_values", "wb") as f:
+            pickle.dump(norm_vals, f)
+    return normalized_samples
+
+def load_raw_audio(data_path, n_train_data, model_path, folders = False):
+    audio = []
+    if folders:
+        for folder in os.listdir(data_path):
+            path = os.path.join(data_path, folder)
+            for file in os.listdir(path):
+                file_path = os.path.join(path, file)
+                signal, sr = lb.load(file_path, sr= 16000) #loading in at 16KHz sampling rate
+                #22050
+                signal = set_duration(signal, max = 16384)
+                audio.append(signal)
+            print(f"Loaded audio from {folder}")
+    else:
+        for file in os.listdir(data_path):
+                file_path = os.path.join(data_path, file)
+                signal, sr = lb.load(file_path)
+                signal = set_duration(signal, max = 16384)
+                audio.append(signal)
+    print(f"Loaded audio from {data_path}")
+
+    audio = np.array(audio)
+    print(f"obtained {len(audio)} samples")
+    audio = audio[:n_train_data]
+    print(f"reduced to {n_train_data} training samlples")
+
+    audio = z_score_normalise(audio, model_path)
+    print("normalised")
+    random.shuffle(audio)
+    
+    audio = np.expand_dims(audio, axis=-1)
+    return audio
+
+
+
---
+++ winfoGAN/train.py
@@ -0,0 +1,90 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from tensorflow.keras import (
+    layers,
+    models,
+    callbacks,
+    losses,
+    utils,
+    metrics,
+    optimizers,
+)
+import librosa as lb
+from model import GAN
+from preprocess import load_raw_audio
+import datetime
+DIM = 64
+CHANNELS = 1 #keeping as 1? what for mono or stereo?
+PHASE_PARAM = 2
+LATENT_DIM = 100
+DISCRIMINATOR_STEPS = 5
+GP_WEIGHT = 10
+LEARNING_RATE = 1e-4
+ADAM_BETA_1 = 0.5
+ADAM_BETA_2 = 0.9
+BATCH_SIZE = 64
+EPOCHS = 3000
+if tf.config.list_physical_devices('GPU'):
+  print("TensorFlow **IS** using the GPU")
+else:
+  print("TensorFlow **IS NOT** using the GPU")
+
+wavegan = GAN(
+    latent_dim = LATENT_DIM,
+    discriminator_steps= DISCRIMINATOR_STEPS,
+    gp_weight= GP_WEIGHT,
+    n_categories= 10
+)
+
+wavegan.compile(
+    d_optimizer = optimizers.Adam(learning_rate=LEARNING_RATE, beta_1 = ADAM_BETA_1, beta_2 = ADAM_BETA_2),
+    g_optimizer = optimizers.Adam(learning_rate=LEARNING_RATE, beta_1 = ADAM_BETA_1, beta_2 = ADAM_BETA_2),
+    q_optimizer= optimizers.RMSprop(learning_rate = LEARNING_RATE)
+)
+
+model_checkpoint_callback = callbacks.ModelCheckpoint(
+    filepath="checkpoints/checkpoint_epoch-{epoch:02d}.hdf5",
+    save_weights_only=False,
+    save_freq="epoch",
+    monitor="loss",
+    mode="min",
+    save_best_only=True,
+    verbose=1,
+)
+time = datetime.datetime.now().strftime("%d%m.%H%M")
+model_path = f"/mt/home/jdave/onedrive/models_{time}"
+os.mkdir(model_path)
+
+tensorboard_callback = callbacks.TensorBoard(log_dir=f"{model_path}/logs")
+
+
+
+#path = r"C:\Users\Jayde\Desktop\Datasets\sc09\sc09"
+
+#path = "/home/jayden/sc09/train"
+path = "/mt/home/jdave/datasets/sc09/sc09/train"
+train_data = load_raw_audio(path, n_train_data= 640, model_path= model_path)
+
+
+wavegan.fit(
+    train_data,
+    batch_size = BATCH_SIZE,
+    shuffle = True,
+    epochs=EPOCHS,
+    callbacks=[
+        model_checkpoint_callback,
+        tensorboard_callback
+    ],
+)
+#input = tf.TensorSpec(tf.random.normal(shape=(None, LATENT_DIM)))
+#wavegan.build((0,100)) #train data or is it looking for the normal distribution?
+#tf.saved_model.save(wavegan,"wavegan")
+#wavegan.save("./models/vae")
+
+wavegan.generator.save(f"{model_path}/generator")
+#wavegan.discriminator.save(f"{model_path}/discriminator")
+print("models saved")
+print("complete")
+
