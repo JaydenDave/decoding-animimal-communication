bde2a924a8130bbb4870fdb09cd82ef36d891aab
Jayden Dave <jaydendave@hotmail.co.uk>
2024-02-09 13:16:39 +0000

    added ate to cdev

--- cdev.py
+++ cdev.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 import tensorflow as tf
+from collections import defaultdict
 #from utils import *
 
 parser = argparse.ArgumentParser()
@@ -21,10 +22,10 @@ parser.add_argument(
     )
 
 parser.add_argument(
-        '--epoch',
-        type=str,
-        default = "",
-        help='generator epoch'
+        '--epochs',
+        type=list,
+        default = ["1000","2000","3000"],
+        help='generator epochs to loop through'
     )
 
 parser.add_argument(
@@ -40,11 +41,20 @@ parser.add_argument(
         default = 0,
         help='baseline_dose'
     )
-args = parser.parse_args()
-epoch = args.epoch
 
+parser.add_argument(
+        '--sr',
+        type=int,
+        default = 24414,
+        help='sampling rate of audio'
+    )
+
+args = parser.parse_args()
+epochs = args.epochs
+sr = args.sr
 NUM = args.num
 BASELINE_DOSE = args.baseline
+
 #load in model
 model_directory =args.model
 spec_path = f"{model_directory}/model_specifications.json"
@@ -61,33 +71,41 @@ gan = GAN(
 
 generator = gan.generator
 
-
-
-
-epochs=[]
-
-sr = 24414
-
 dose_vals =np.arange(-1, 13, 0.5)
-#generate z part which will stay the same for all
 
+#generate z part which will stay the same for all
 latent_dim = specs["Latent Dim"]
 n_cat = specs["N Categories"]
 bits = range(n_cat)
 z_dim = latent_dim-n_cat
 z= np.random.normal(size=(NUM, latent_dim))
-z[:,z_dim:]= BASELINE_DOSE
 
-for epoch in ["300","600","1400"]:
-    df = pd.DataFrame()
+functions=[avg_fundamental_freq,]
+function_labels=["F0"]
+
+for epoch in epochs:
+    
     all_inputs = []
-    all_f0 =[]
+    all_data=defaultdict(list)
+    
     doses = []
     bit_vals = []
-    fundamental_freqs = []
-    f0_stds = []
+    outputs=defaultdict(list)
+
     generator.load_weights(f"{model_directory}/generator{epoch}")
     epoch = "5000" if epoch =="" else epoch
+
+    #generate baseline for ate
+    z[:,z_dim:]= BASELINE_DOSE
+    input= tf.convert_to_tensor(z, dtype=tf.float32)
+    generated_audio = generator.predict(input)
+    generated= np.squeeze(generated_audio)
+    baselines=[]
+
+    for function in functions:
+        results= function(generated, sr = sr)
+        baselines.append(results)
+
     for dose in tqdm(dose_vals):
         
         for bit in bits:
@@ -95,27 +113,30 @@ for epoch in ["300","600","1400"]:
             z[:,z_dim+bit]=dose
             input= tf.convert_to_tensor(z, dtype=tf.float32)
 
-
-            all_inputs += list(input.numpy())
             generated_audio = generator.predict(input)
             generated= np.squeeze(generated_audio)
-            f0s = avg_fundamental_freq(generated, sr = sr)
-            all_f0 += f0s
-            f0_avg = np.average(f0s)
-            f0_std = np.std(f0s)
-            
 
             doses.append(dose)
             bit_vals.append(bit)
-            fundamental_freqs.append(f0_avg)
-            f0_stds.append(f0_std)
+            all_inputs += list(input.numpy())
+
+            #apply acoustic proprty finding algorithms to generated samples
+            for function, name, baseline in zip(functions, function_labels, baselines):
+                results= function(generated, sr = sr)
+                all_data[name] += results
+                ates= [x-y for x,y in zip(results, baseline)]
+                
+                outputs[f"{name}_avg"].append(np.average(ates))
+                outputs[f"{name}_std"].append(np.std(ates))
 
     col_names = [f"z_{num}" for num in range(z_dim)] + [f"bit_{num}" for num in range(specs["N Categories"])]
     raw_data = pd.DataFrame(all_inputs, columns=col_names)
-    raw_data["F0"] = all_f0
-    df['bit'] = bit_vals
-    df['dose'] = doses
-    df['f0'] = fundamental_freqs
-    df['f0 std'] = f0_stds
+    raw_data_results = pd.DataFrame(all_data)
+    raw_data = pd.concat([raw_data,raw_data_results], axis=1)
+
+    df = pd.DataFrame({"bit":bit_vals, "dose":doses})
+    results = pd.DataFrame(outputs)
+    df = pd.concat([df,results], axis=1)
+
     df.to_csv(f"{model_directory}/Bits{epoch}.csv", index= False)
     raw_data.to_csv(f"{model_directory}/raw_data{epoch}.csv", index= False)
